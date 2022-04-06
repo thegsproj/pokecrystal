6b1962629ef5c418d3fc10fa6abb7fc9c120d497
vulcandth <vulcandth@gmail.com>
2022-04-05 21:28:25 -0500

    New-Game-Marill

--- engine/menus/intro_menu.asm
+++ engine/menus/intro_menu.asm
@@ -657,7 +657,7 @@ OakSpeech:
 	call RotateThreePalettesRight
 	call ClearTilemap
 
-	ld a, WOOPER
+	ld a, MARILL
 	ld [wCurSpecies], a
 	ld [wCurPartySpecies], a
 	call GetBaseData
@@ -717,7 +717,7 @@ OakText1:
 OakText2:
 	text_far _OakText2
 	text_asm
-	ld a, WOOPER
+	ld a, MARILL
 	call PlayMonCry
 	call WaitSFX
 	ld hl, OakText3
